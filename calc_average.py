6211d3d879624ae79fc2a87666f1ab64632bce94
kojima0 <44964860+kojima0@users.noreply.github.com>
2018-11-22 20:09:21 +0900

    Create calc_average.py
    平均点を求める

---
+++ calc_average.py
@@ -0,0 +1,15 @@
+total_points = 0
+total_people = 0
+
+
+while True:
+    point = int(input("点数を入力してください："))
+
+    if point == -1:
+        break
+    
+    total_points = total_points + point
+    total_people += 1
+    
+average = total_points / total_people
+print(str(total_people),"人のテストの平均点は",str(average),"点です")
