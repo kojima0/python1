e8450d260bac1185951ff415b63e3a2a32c7c39d
kojima0 <44964860+kojima0@users.noreply.github.com>
2018-11-22 20:10:40 +0900

    Create prime_number.py
    エラトステネスのふるいを実装する

---
+++ prime_number.py
@@ -0,0 +1,14 @@
+#エラトステネスのふるい
+#素数を表示する
+for i in range(1,100):
+    if i == 1:
+        continue
+    if i != 2 and i % 2 == 0:
+        continue
+    if i != 3 and i % 3 == 0:
+        continue
+    if i != 5 and i % 5 == 0:
+        continue
+    if i != 7 and i % 7 == 0:
+        continue
+    print(i)
